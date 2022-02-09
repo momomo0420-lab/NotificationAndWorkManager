4614f30cf673e42cbb97544fce62939a4c7ecff6
momomo0420-lab <momomo0420.lab+github@gmail.com>
2022-02-09 21:37:52 +0900

    Implemented WorkManager.

--- app/src/main/java/com/example/notificationandworkmanager/MainActivity.kt
+++ app/src/main/java/com/example/notificationandworkmanager/MainActivity.kt
@@ -1,84 +1,36 @@
 package com.example.notificationandworkmanager
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.work.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val CHANNEL_ID = "NOTIFICATION_CHANNEL_TEST_ID"
-    }
-
-    private lateinit var myNotification: Notification
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myNotification = createNotification()
-
-        // 通知用チャンネルを登録
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        val channel = createMyNotificationChannel()
-        manager.createNotificationChannel(channel)
-
         // ボタン押下時の処理登録
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener(myOnClickListener)
-
-        // ワークマネージャーの登録
-        val workRequest = createWorkerRequest()
-        val workManager = WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-            "test_notification",
-            ExistingPeriodicWorkPolicy.KEEP,
-            workRequest
-        )
     }
 
     // ボタン押下時の処理（リスナー）
     private val myOnClickListener = View.OnClickListener {
-        NotificationManagerCompat
-            .from(this)
-            .notify(100, myNotification)
-    }
-
-    /**
-     * 通知用のチャンネルを作成
-     *
-     * @return 通知用のチャンネル
-     */
-    private fun createMyNotificationChannel(): NotificationChannel {
-        return NotificationChannel(
-            CHANNEL_ID,
-            "Notification channel test",
-            NotificationManager.IMPORTANCE_DEFAULT
-        ).apply {
-            description = "AAAAAAA"
-        }
-    }
-
-    /**
-     * 通知の作成
-     *
-     * @return 通知
-     */
-    private fun createNotification() : Notification {
-        return NotificationCompat
-            .Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_notification)
-            .setContentTitle("My Notification")
-            .setContentText("Notification Practice")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
+        // ワークマネージャーの登録
+        val workRequest = createWorkerRequest()
+        WorkManager
+            .getInstance(this)
+            .enqueueUniquePeriodicWork(
+                "test_notification",
+                ExistingPeriodicWorkPolicy.KEEP,
+                workRequest
+            )
     }
 
     /**
--- app/src/main/java/com/example/notificationandworkmanager/MyWorker.kt
+++ app/src/main/java/com/example/notificationandworkmanager/MyWorker.kt
@@ -1,6 +1,11 @@
 package com.example.notificationandworkmanager
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
@@ -9,7 +14,56 @@ class MyWorker(
     params: WorkerParameters
 ) : Worker(context, params) {
 
+    companion object {
+        private const val CHANNEL_ID = "NOTIFICATION_CHANNEL_TEST_ID"
+    }
+
+    private val context = applicationContext
+
+    private lateinit var myNotification: Notification
+
     override fun doWork(): Result {
+        myNotification = createNotification()
+
+        // 通知用チャンネルを登録
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = createMyNotificationChannel()
+        manager.createNotificationChannel(channel)
+
+        NotificationManagerCompat
+            .from(context)
+            .notify(100, myNotification)
+
         return Result.success()
+    }
+
+    /**
+     * 通知用のチャンネルを作成
+     *
+     * @return 通知用のチャンネル
+     */
+    private fun createMyNotificationChannel(): NotificationChannel {
+        return NotificationChannel(
+            CHANNEL_ID,
+            "Notification channel test",
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = "AAAAAAA"
+        }
+    }
+
+    /**
+     * 通知の作成
+     *
+     * @return 通知
+     */
+    private fun createNotification() : Notification {
+        return NotificationCompat
+            .Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle("My Notification")
+            .setContentText("Notification Practice")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
     }
 }
